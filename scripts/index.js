cbeb29d8a8b1a7059d3b3584c6367676339a1995
Nikolay Vyglazov <nikvyglazov@yandex.ru>
2022-05-26 00:40:05 +0100

    Final version afer first review

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,39 @@
 const root = document.querySelector('.page');
-const profile = root.querySelector('.profile')
-const profileName = profile.querySelector('.profile__info-title');
-const profileAbout = profile.querySelector('.profile__info-subtitle');
+
+const profile = root.querySelector('.profile');
+const profileInfo = profile.querySelector('.profile__info');
+const btnEditProfile = profileInfo.querySelector ('.profile__edit-button');
+const profileName = profileInfo.querySelector('.profile__info-title');
+const profileAbout = profileInfo.querySelector('.profile__info-subtitle');
+
+// объявляем секцию для popup и модального окна Редактирования Профиля
 const popup = root.querySelector('.popup');
 const popupEditProfile = root.querySelector('.popup__edit-prolie');
-
-const btnEditProfile = profile.querySelector ('.profile__edit-button');
-const btnCloseEditProfile = popupEditProfile.querySelector('.popup__close-button');
-const btnSaveEditProfile = popupEditProfile.querySelector('#btnOK-frmEditProfile')
+const frmEditProfile = popupEditProfile.querySelector('.form-edit-profile');
+// поля отображения для Профиля: Имя и О_себе
 const inputProfileName = popupEditProfile.querySelector('#name-input');
 const inputProfileAbout = popupEditProfile.querySelector('#about-input');
+// кнопки для формы Закрыть
+const btnCloseEditProfile = popupEditProfile.querySelector('.popup__close-button');
+// const btnSubmitEditProfile = popupEditProfile.querySelector('.form-edit__submit-button');
 
-btnEditProfile.addEventListener('click', onclickOpenEditProfile);
-btnCloseEditProfile.addEventListener('click', onclickCloseEditProfile);
-btnSaveEditProfile.addEventListener('click', onclickSaveEditProfile);
-
-function onclickOpenEditProfile() {
-  openPopupForm(popup);
+function openEditProfile_OnClick() {
+  popup.classList.add('popup_active');
   inputProfileName.value = profileName.textContent;
   inputProfileAbout.value = profileAbout.textContent;
 };
 
-function onclickCloseEditProfile() {
-  closePopupForm(popup);
+function closeEditProfile_OnClick() {
+  popup.classList.remove('popup_active');
 };
 
-function onclickSaveEditProfile() {
+function submit_frmEditProfile(evt) {
+  evt.preventDefault();
   profileName.textContent = inputProfileName.value;
   profileAbout.textContent = inputProfileAbout.value;
-  onclickCloseEditProfile();
+  popup.classList.remove('popup_active');
 };
 
-function openPopupForm(popupForm) {
-  popupForm.classList.add('popup_active');
-};
-
-function closePopupForm(popupForm) {
-  popupForm.classList.remove('popup_active');
-};
-
+btnEditProfile.addEventListener('click', openEditProfile_OnClick);
+btnCloseEditProfile.addEventListener('click', closeEditProfile_OnClick);
+frmEditProfile.addEventListener('submit', submit_frmEditProfile)
